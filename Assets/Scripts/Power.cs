70c7cbf4145269407ba774ef96396f59ddd3fce5
Randgriz11 <88016673+Randgriz11@users.noreply.github.com>
2022-12-10 14:19:42 -0800

    Add files via upload

---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    private float power =50f;
+    public GameObject ballformControl;
+    public Vector3 direction = new Vector3(.45f, .45f, .45f);
+    private int counter=1;
+    private bool collided = false;
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = power * direction;
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        power = ballformControl.GetComponent<BallFormControl>().power;
+    }
+    
+    
+    private void onCollisionEnter(Collision collision)
+    {
+        // Check if the sphere has collided with the floor
+        if (collision.gameObject.tag == "Walle")
+        {
+            collided = true;
+        }
+    }
+}
